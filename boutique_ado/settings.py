1e7446a4f83f9c21733f6b719a5ec46335f71618
IsaHu-dev <usunisa.hu@students.codeinstitute.net>
2025-04-19 18:18:44 +0100

    add cover image to index page and styling

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -149,6 +149,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),) # Add this line to include the static directory
+
+MEDIA_URL='/media/'
+MEDIA_ROOT=os.path.join(BASE_DIR,'media') # Add this line to include the media directory
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field

fdd2a2106eca0ec2c8cabc001ec2f6c1ca1fc238
IsaHu-dev <usunisa.hu@students.codeinstitute.net>
2025-04-22 12:51:11 +0100

    Fix category filtering in products view

--- products/views.py
+++ products/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Product
+from django.db. models import Q
+from .models import Product, Category
 
 # Create your views here.
 
@@ -7,9 +8,27 @@ def all_products(request):
     """ A view to show all products, including sorting and search queries """
 
     products = Product.objects.all()
-
+    query = None
+    categories = None
+    
+    if request.GET:
+        if 'q' in request.GET:
+            categories = request.GET.getlist('category') . split(',')
+            products = products.filter(category__name__in=categories)
+            categories = Catogory,objects.filter(name__in=categories)
+            
+            query = request.GET['q']
+            if not query:
+                message.error(request, "You didn't enter any search criteria!")
+                return redirect(reverse('products'))
+            
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            products = products.filter(queries)
+            
     context = {
         'products': products,
+        'search_term': query,
+        'current_categories': categories,
     }
 
     return render(request, 'products/products.html', context)

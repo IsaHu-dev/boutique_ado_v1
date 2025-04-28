387d75a3fa23e14e4a3a23745d2e23cb6c8eb138
IsaHu-dev <usunisa.hu@students.codeinstitute.net>
2025-04-28 18:28:21 +0100

    add stripe settings to settings.py, views.py and js script

--- checkout/signals.py
+++ checkout/signals.py
@@ -9,8 +9,12 @@ def update_on_save(sender, instance, created, **kwargs):
     Update order total on lineitem update/create
     """
     instance.order.update_total()
+
+
 @receiver(post_delete, sender=OrderLineItem)
 def update_on_save(sender, instance, **kwargs):
+    # chris made a mistake here, this function should have 
+    # been called update_on_delete. He fixes this in an upcoming video.
     """
     Update order total on lineitem delete
     """

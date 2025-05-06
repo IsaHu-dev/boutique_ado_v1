713d3355a37bb839fd0a2c5af66d84f83accc09b
IsaHu-dev <usunisa.hu@students.codeinstitute.net>
2025-05-06 17:58:15 +0100

    real email sending

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -216,11 +216,12 @@ if 'DEVELOPMENT' in os.environ:
     DEFAULT_FROM_EMAIL = 'izzysocial25@gmail.com'
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-    EMAIL_HOST = 'smtp.gmail.com'
-    EMAIL_PORT = 587
     EMAIL_USE_TLS = True
+    EMAIL_PORT = 587
+    EMAIL_HOST = 'smtp.gmail.com'
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
     EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
-    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
+    DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL')
+ 
     
     
